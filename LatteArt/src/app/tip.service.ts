63aa94fcc09662f07a2d41035591487510d5beae
SapientCoffee <rob.j.edwards@gmail.com>
2025-03-07 22:12:58 -0800

    initial coffee theme

--- LatteArt/src/app/tip.service.ts
+++ LatteArt/src/app/tip.service.ts
@@ -1,26 +1,27 @@
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
-import {catchError, map} from 'rxjs/operators';
-
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class TipService {
-    private apiUrl = 'https://localhost:3000/api/v1/tips/generate';
+  // If you are testing, change this to http.
+  private apiUrl = 'http://localhost:3000/api/v1/tips/generate';
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {}
 
-    getTip(): Observable<string> {
-        return this.http.get<any>(this.apiUrl).pipe(
-            map(response => response.tip),
-            catchError(error => {
-                console.error('Error fetching tip:', error);
-                return of('Failed to load tip of the day.');
-            })
-        );
-    }
-
+  getTip(): Observable<string> {
+    return this.http.get<any>(this.apiUrl).pipe(
+      map((response) => response.tip),
+      catchError((error: HttpErrorResponse) => { 
+        console.error('Error fetching tip:', error);
+        if(error.status == 0){
+            return of('The tip server is not running, please contact support.');
+        }
+        return of('Failed to load tip of the day.');
+      })
+    );
+  }
 }
