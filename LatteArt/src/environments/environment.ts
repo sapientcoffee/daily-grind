d93f4e9873740622ea5364defd4be2bf5c4787f4
robedwards <robedwards@google.com>
2025-03-14 22:26:07 +0000

    app hosting initial deployment settings

---
+++ LatteArt/src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+    firebase: {
+        apiKey: "12345",
+        authDomain: "cymbal-daily-grind.firebaseapp.com",
+        projectId: "cymbal-daily-grind",
+        storageBucket: "cymbal-daily-grind.firebasestorage.app",
+        messagingSenderId: "1027739766041",
+        appId: "1:1027739766041:web:9c955a567d5a7826a2c988",
+        measurementId: "G-M611D4SKZD"
+    },
+  };
